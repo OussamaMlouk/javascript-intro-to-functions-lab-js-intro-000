61f930e2eb1b91da12f03b8cb6c15905be30547f
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-09 22:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+var lowercase = 'hello!'
+var uppercase = "HELLO!"
+
+function sayHiToGrandma(string){
+  if string === lowercase{
+    return `I can\'t hear you!`
+  }
+}
